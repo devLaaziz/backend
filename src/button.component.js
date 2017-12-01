f654f964bb44d0fa97d9f10bdde2b473fe457cf0
Ayoub Laaziz <a.laaziz@piman2-0.fr>
2017-12-01 20:55:44 +0000

    Initial commit

---
+++ src/button.component.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  ViewPropTypes,
+} from 'react-native';
+import styles from './button.style';
+
+export default class BestButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+}
+
+BestButton.defaultProps = {
+    
+};
+
+BestButton.propTypes = {
+
+}
